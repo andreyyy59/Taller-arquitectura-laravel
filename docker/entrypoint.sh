498649111e1b4a3b1068135722b6c9665d5b27b4
andreymorales <amorales96@unab.edu.co>
2026-04-23 08:51:51 -0500

    Fix: Corrigiendo conexion a DB y añadiendo auto-seed en Render

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,10 +9,10 @@ fi
 php artisan config:cache
 
 databaseConnection=$(php artisan tinker --execute="echo config('database.default')")
-databaseHost=$(php artisan tinker --execute="echo config('database.connections.' . '$databaseConnection' . '.host')")
-databasePort=$(php artisan tinker --execute="echo config('database.connections.' . '$databaseConnection' . '.port')")
+databaseHost=$(php artisan tinker --execute="echo config('database.connections.' . $databaseConnection . '.host')")
+databasePort=$(php artisan tinker --execute="echo config('database.connections.' . $databaseConnection . '.port')")
 
-./docker/wait-for-it.sh $databaseHost:$databasePort -t 90 -- php artisan migrate --force
+./docker/wait-for-it.sh $databaseHost:$databasePort -t 90 -- php artisan migrate --force --seed
 
 supervisord -n -c docker/supervisord.conf
 
